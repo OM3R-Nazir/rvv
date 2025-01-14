855da617e83cb637a8fa35c60e013081fb814054
Eagle <omeagle.101@gmail.com>
2025-01-14 22:50:08 +0500

    removed main.py

--- rvv/__init__.py
+++ rvv/__init__.py
@@ -1,3 +1,7 @@
-from rvv.main import RVV
+from rvv.integer import RVVInteger
+
+class RVV(RVVInteger):
+    def __init__(self, VLEN=2048, debug=False):
+        super().__init__(VLEN, debug)
 
 __all__ = ['RVV']
--- rvv/main.py
+++
@@ -1,5 +0,0 @@
-from rvv.integer import RVVInteger
-
-class RVV(RVVInteger):
-    def __init__(self, VLEN=2048, debug=False):
-        super().__init__(VLEN, debug)
