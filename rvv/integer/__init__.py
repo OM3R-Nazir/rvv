3d5a34d1ae556b45aa5ce91aaa3316b03d8db93d
Eagle <omeagle.101@gmail.com>
2025-01-19 17:51:14 +0500

    Added mulltiply-add funcs

--- rvv/integer/__init__.py
+++ rvv/integer/__init__.py
@@ -3,8 +3,9 @@ from rvv.integer.subtract import SUBTRACT
 from rvv.integer.multiply import MULTIPLY
 from rvv.integer.compare import COMPARE
 from rvv.integer.carry import CARRY
+from rvv.integer.muladd import MULADD
 
-class RVVInteger(ADD, SUBTRACT, MULTIPLY, COMPARE, CARRY):
+class RVVInteger(ADD, SUBTRACT, MULTIPLY, COMPARE, CARRY, MULADD):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
---
+++ rvv/integer/muladd.py
@@ -0,0 +1,89 @@
+from rvv.base import BaseRVV
+import numpy as np
+
+class MULADD(BaseRVV):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    ##
+    ## Same Width
+    ##
+    
+    def vmacc_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', False, masked)
+        vvd[mask] = ((vs1 * vs2) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vmacc_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', False, masked)
+        vvd[mask] = ((rs1 * vs2) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vnmsac_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', False, masked)
+        vvd[mask] = (-(vs1 * vs2) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vnmsac_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', False, masked)
+        vvd[mask] = (-(rs1 * vs2) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vmadd_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', False, masked)
+        vvd[mask] = ((vvd * vs1) + vs2)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vmadd_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', False, masked)
+        vvd[mask] = ((vvd * rs1) + vs2)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vnmsub_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', False, masked)
+        vvd[mask] = (-(vvd * vs1) + vs2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vnmsub_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', False, masked)
+        vvd[mask] = (-(vvd * rs1) + vs2)[mask]
+        self._debug_vd(vvd, vd)
+        
+    ##
+    ## Widening
+    ##
+    
+    def vwmacc_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'wvv', True, masked)
+        vvd[mask] = ((self._sext(vs1) * self._sext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vwmacc_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', True, masked)
+        vvd[mask] = ((self._sext(rs1) * self._sext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vwmaccsu_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', 'ssu', masked)
+        vvd[mask] = ((self._sext(vs1) * self._zext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vwmaccsu_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', 'ssu', masked)
+        vvd[mask] = ((self._sext(rs1) * self._zext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vwmaccus_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', 'usu', masked)
+        vvd[mask] = ((self._zext(rs1) * self._sext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vwmaccu_vv(self, vd, vs1, vs2, masked):
+        vvd, vs1, vs2, mask = self._init_ops(vd, vs1, vs2, 'vvv', False, masked)
+        vvd[mask] = ((self._zext(vs1) * self._zext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vwmaccu_vx(self, vd, rs1, vs2, masked):
+        vvd, rs1, vs2, mask = self._init_ops(vd, rs1, vs2, 'vxv', False, masked)
+        vvd[mask] = ((self._zext(rs1) * self._zext(vs2)) + vvd)[mask]
+        self._debug_vd(vvd, vd)
