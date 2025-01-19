f88d8a320883d31cd294aa23d2d98beb77f1cf8b
Eagle <omeagle.101@gmail.com>
2025-01-19 19:01:04 +0500

    Added int/misc funcs - contains: vmin, vmax, vzext, zsext, vneg

--- rvv/integer/__init__.py
+++ rvv/integer/__init__.py
@@ -5,8 +5,9 @@ from rvv.integer.divide import DIVIDE
 from rvv.integer.compare import COMPARE
 from rvv.integer.carry import CARRY
 from rvv.integer.muladd import MULADD
+from rvv.integer.misc import MISC
 
-class RVVInteger(ADD, SUBTRACT, MULTIPLY, DIVIDE, COMPARE, CARRY, MULADD):
+class RVVInteger(ADD, SUBTRACT, MULTIPLY, DIVIDE, COMPARE, CARRY, MULADD, MISC):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
---
+++ rvv/integer/misc.py
@@ -0,0 +1,102 @@
+from rvv.base import BaseRVV
+import numpy as np
+
+class MISC(BaseRVV):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    ##
+    ## MIN
+    ##
+    
+    def vmin_vv(self, vd, op1, op2, masked):
+        vvd, vop1, vop2, mask = self._init_ops(vd, op1, op2, 'vvv', True, masked)
+        vvd[mask] = np.minimum(vop1, vop2)[mask]
+        self._debug_vd(vvd, vd)
+
+    def vmin_vx(self, vd, op1, op2, masked):
+        vvd, vop1, xop2, mask = self._init_ops(vd, op1, op2, 'vvx', True, masked)
+        vvd[mask] = np.minimum(vop1, xop2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vminu_vv(self, vd, op1, op2, masked):
+        vvd, vop1, vop2, mask = self._init_ops(vd, op1, op2, 'vvv', False, masked)
+        vvd[mask] = np.minimum(vop1, vop2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vminu_vx(self, vd, op1, op2, masked):
+        vvd, vop1, xop2, mask = self._init_ops(vd, op1, op2, 'vvx', False, masked)
+        vvd[mask] = np.minimum(vop1, xop2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    ##
+    ## MAX
+    ##
+    
+    def vmax_vv(self, vd, op1, op2, masked):
+        vvd, vop1, vop2, mask = self._init_ops(vd, op1, op2, 'vvv', True, masked)
+        vvd[mask] = np.maximum(vop1, vop2)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vmax_vx(self, vd, op1, op2, masked):
+        vvd, vop1, xop2, mask = self._init_ops(vd, op1, op2, 'vvx', True, masked)
+        vvd[mask] = np.maximum(vop1, xop2)[mask]
+        self._debug_vd(vvd, vd)
+        
+    def vmaxu_vv(self, vd, op1, op2, masked):
+        vvd, vop1, vop2, mask = self._init_ops(vd, op1, op2, 'vvv', False, masked)
+        vvd[mask] = np.maximum(vop1, vop2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    def vmaxu_vx(self, vd, op1, op2, masked):
+        vvd, vop1, xop2, mask = self._init_ops(vd, op1, op2, 'vvx', False, masked)
+        vvd[mask] = np.maximum(vop1, xop2)[mask]
+        self._debug_vd(vvd, vd)
+    
+    ##
+    ## Negate
+    ##
+    
+    def vneg_v(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_uni(vd, op1, 'vv', True, masked)
+        vvd[mask] = -vop1[mask]
+        self._debug_vd(vvd, vd)
+
+    ##
+    ## Zero Extend
+    ##
+    
+    def vzext_vf2(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf2', False, masked)
+        vvd[mask] = self.SEW.udtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+    
+    def vzext_vf4(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf4', False, masked)
+        vvd[mask] = self.SEW.udtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+    
+    def vzext_vf8(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf8', False, masked)
+        vvd[mask] = self.SEW.udtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+    
+    ##
+    ## Sign Extend
+    ##
+    
+    def vsext_vf2(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf2', True, masked)
+        vvd[mask] = self.SEW.dtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+    
+    def vsext_vf4(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf4', True, masked)
+        vvd[mask] = self.SEW.dtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+    
+    def vsext_vf8(self, vd, op1, masked):
+        vvd, vop1, mask = self._init_ops_ext(vd, op1, 'vf8', True, masked)
+        vvd[mask] = self.SEW.dtype(vop1[mask])
+        self._debug_vd(vvd, vd)
+        
