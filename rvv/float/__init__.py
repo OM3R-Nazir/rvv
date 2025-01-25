b9de68d50ae2e066ad3c78ed9cf2b91ad8f5194b
Eagle <omeagle.101@gmail.com>
2025-01-25 21:31:23 +0500

    added fcompare

--- rvv/float/__init__.py
+++ rvv/float/__init__.py
@@ -1,6 +1,7 @@
 
 from rvv.float.basicfops import BASICOPS
+from rvv.float.fcompare import FCompare
 
-class RVVFloat(BASICOPS):
+class RVVFloat(BASICOPS, FCompare):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
