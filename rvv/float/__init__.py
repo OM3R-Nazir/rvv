a8eaab8e010d936c1cd42d856a1629e1ae13df39
Eagle <omeagle.101@gmail.com>
2025-01-25 22:12:05 +0500

    added fmuladd

--- rvv/float/__init__.py
+++ rvv/float/__init__.py
@@ -2,7 +2,8 @@
 from rvv.float.fopsbasic import FOpsBasic
 from rvv.float.fcompare import FCompare
 from rvv.float.fwiden import FWiden
+from rvv.float.fmuladd import FMulAdd
 
-class RVVFloat(FOpsBasic, FCompare, FWiden):
+class RVVFloat(FOpsBasic, FCompare, FWiden, FMulAdd):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
