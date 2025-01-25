306db36d3b19e9d6170c7417531a18f3b8605719
Eagle <omeagle.101@gmail.com>
2025-01-25 21:33:10 +0500

    added fwiden

--- rvv/float/__init__.py
+++ rvv/float/__init__.py
@@ -1,7 +1,8 @@
 
 from rvv.float.basicfops import BASICOPS
 from rvv.float.fcompare import FCompare
+from rvv.float.fwiden import FWiden
 
-class RVVFloat(BASICOPS, FCompare):
+class RVVFloat(BASICOPS, FCompare, FWiden):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
