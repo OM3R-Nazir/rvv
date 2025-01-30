d7cd918d67085eef7cb4c0fd2127fdfbcd2791c1
Eagle <omeagle.101@gmail.com>
2025-01-30 22:10:05 +0500

    added bitwise operations

--- rvv/__init__.py
+++ rvv/__init__.py
@@ -1,8 +1,9 @@
 from rvv.integer import RVVInteger
 from rvv.float import RVVFloat
 from rvv.mask import RVVMask
+from rvv.bitwise import RVVBitwise
 
-class RVV(RVVInteger, RVVFloat, RVVMask):
+class RVV(RVVInteger, RVVFloat, RVVMask, RVVBitwise):
     def __init__(self, VLEN: int = 2048, debug = False, debug_vb_as_v = False):
         super().__init__(VLEN, debug, debug_vb_as_v)
 
---
+++ rvv/bitwise/__init__.py
@@ -0,0 +1,5 @@
+from rvv.bitwise.shift import Shift
+from rvv.bitwise.logical import Logical
+
+class RVVBitwise(Shift, Logical):
+    pass
